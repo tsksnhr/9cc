c0184c58c123ee9a27bb4cf92cc81990e369e0e5
tsksnhr <toshiki.o3o5.s@gmail.com>
2021-08-23 16:19:15 +0000

    add features (mul, div, and brackets) and prototype declaration

--- 9cc.c
+++ 9cc.c
@@ -1,3 +1,4 @@
+// header
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +12,14 @@ typedef enum {
 	TK_EOF,		// End Of File
 } TokenKind;
 
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
 typedef struct Token Token;
 
 struct Token {
@@ -19,6 +28,31 @@ struct Token {
 	int val;
 	char *str;	// Token-type string
 };
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+    char *str;  // operand
+};
+
+// prototype declaraiton
+void error_at(char *loc, char *fmt, ...);
+bool consume(char op);
+void expect(char op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str);
+Token *tokenize(char *p);
+Node *new_node(NodeKind kind, Node *lhs, Node *rsh);
+Node *new_node_num(int val);
+Node *expr();
+Node *mul();
+Node *primary();
+void gen(Node *node);
 
 // Token that be focused on
 Token *token;
@@ -97,7 +131,7 @@ Token *tokenize(char *p){
 			continue;
 		}
 
-		if (*p == '+' || *p == '-'){
+		if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -115,6 +149,93 @@ Token *tokenize(char *p){
 	return head.next;
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *primary(){
+    if (consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
+
+Node *mul(){
+    Node *node = primary();
+
+    for(;;){
+        if (consume('*')){
+            node = new_node(ND_MUL, node, primary());
+        }
+        else if (consume('/')){
+            node = new_node(ND_DIV, node, primary());
+        }
+        else{
+            return node;
+        }
+    }
+}
+
+Node *expr(){
+    Node *node = mul();
+
+    for(;;){
+        if (consume('+')){
+            node = new_node(ND_ADD, node, mul());
+        }
+        else if (consume('-')){
+            node = new_node(ND_SUB, node, mul());
+        }
+        else{
+            return node;
+        }
+    }
+}
+
+void gen(Node *node){
+    if (node->kind == ND_NUM){
+        printf("	push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("	pop rdi\n");
+    printf("	pop rax\n");
+
+    switch (node->kind){
+        case ND_ADD:
+            printf("	add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("	sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("	imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("	cqo\n");
+            printf("	idiv rdi\n");
+            break;
+    }
+
+    printf("	push rax\n");
+}
 
 int main(int argc, char **argv){
 
@@ -131,18 +252,10 @@ int main(int argc, char **argv){
 	printf(".global main\n");
 	printf("main:\n");
 
-	printf("	mov rax, %d\n", expect_number());
+	Node *node = expr();
+	gen(node);
 
-	while (!at_eof()){
-		if (consume('+')){
-			printf("	add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("	sub rax, %d\n", expect_number());
-	}
-
+	printf("	pop rax\n");
 	printf("	ret\n");
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -5,9 +5,9 @@ assert(){
 	expected="$1"
 	input="$2"
 
-	/home/9cc/9cc "$input" > tmp.s
+	/workspace/9cc/9cc "$input" > tmp.s
 	cc -o tmp tmp.s
-	/home/9cc/tmp
+	/workspace/9cc/tmp
 	actual="$?"
 
 	if [ "$actual" = "$expected" ]; then
@@ -21,7 +21,11 @@ assert(){
 assert 0 0
 assert 42 42
 
-assert 21 "5+20-4"
-assert 46 " 5 + 40 - 2 + 3"
+assert 21 '5+20-4'
+assert 46 ' 5 + 40 - 2 + 3'
+
+assert 26 '2 * 3 + 4 * 5'
+assert 44 '4 * ( 6 + 5 )'
+assert 3 '36 / ( 4 + 8 )'
 
 echo OK
