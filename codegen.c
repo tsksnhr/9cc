5e448b7576f6f6318834a539dcb4f18ff3805ecb
tsksnhr <toshiki.o3o5.s@gmail.com>
2021-09-11 10:28:37 +0000

    delete unnecessary code

--- codegen.c
+++ codegen.c
@@ -30,10 +30,6 @@ void gen(Node *node){
 		case ND_RETURN:
 			gen(node->lhs);
 			printf("	pop rax\n");
-			printf("	mov rsp, rbp\n");
-			printf("	pop rbp\n");
-			printf("	ret\n");
-
 			epilogue();
 
 			return;
